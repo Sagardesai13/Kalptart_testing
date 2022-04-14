76e8a17196119372299a1a811576a26c3243a061
ParthGoti <parthgoti999@gmail.com>
2022-04-14 11:42:45 +0530

    Editorder 2

--- src/Components/EditOrder/edirOrder.jsx
+++ src/Components/EditOrder/edirOrder.jsx
@@ -3,20 +3,20 @@ import { Link } from 'react-router-dom';
 import Navbar from '../NavBar/Navbar';
 import {BiArrowBack} from "react-icons/bi";
 import './editOrder.css';
+import Home from '../Home/Home'
 import { editData } from './editData';
 
 function editOrder() {
     return(
         <>
         <Navbar/>
-        <div className="container">
-        <div className="container edit-text">
-        <Link to='#'>
-        <BiArrowBack className='m-2' style={{color: 'black', height: '28px', width: '28px',top:'24px',left:'24px'}}/>
-        </Link>
+        <div className="edit-text">
+    <Link to='../Home'>
+      <BiArrowBack className='m-2' style={{color: 'black', height: '28px', width: '28px',top:'24px',left:'24px'}}/>
+    </Link>
         <p className='editorder-text'>Edit Order</p>
         </div>
-        </div>
+       
         </>
     )
 }
