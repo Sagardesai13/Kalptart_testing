d99e08542ae8c5d19e8cb668bd24d391c101aaa5
ParthGoti <parthgoti999@gmail.com>
2022-04-14 11:07:06 +0530

    Edit order upadted

--- src/Components/EditOrder/edirOrder.jsx
+++ src/Components/EditOrder/edirOrder.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from '../NavBar/Navbar';
+import {BiArrowBack} from "react-icons/bi";
+import './editOrder.css';
+import { editData } from './editData';
 
 function editOrder() {
     return(
         <>
-            <h1>Hello Edit-Order</h1>
-            <p>Testing</p>
+        <Navbar/>
+        <div className="container">
+        <div className="container edit-text">
+        <Link to='#'>
+        <BiArrowBack className='m-2' style={{color: 'black', height: '28px', width: '28px',top:'24px',left:'24px'}}/>
+        </Link>
+        <p className='editorder-text'>Edit Order</p>
+        </div>
+        </div>
         </>
     )
 }
--- src/Components/EditOrder/editData.jsx
+++ src/Components/EditOrder/editData.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
-import * as GrIcons from 'react-icons/gr';
+import  {FiEdit2}  from "react-icons/fi";
 
 export const editData = [
     {
       cname: 'Harsh Panchal',
       rno: '5304',
-      icon: <GrIcons.GrAddCircle />,
+      icon: <FiEdit2></FiEdit2>,
       cName: 'edit-text'
     },
     {
       cname: 'Nilesh Patel',
       rno: '4497',
-      icon: <GrIcons.GrAddCircle />,
+      icon: <FiEdit2></FiEdit2>,
       cName: 'edit-text'
       },
     {
       cname: 'Gurmeet Singh',
       rno: '3367',
-      icon: <GrIcons.GrAddCircle />,
+      icon:<FiEdit2></FiEdit2>,
       cName: 'edit-text'
       },
     {
       cname: 'Yash Rajput',
       rno: '3367',
-      icon: <GrIcons.GrAddCircle />,
+      icon: <FiEdit2></FiEdit2>,
       cName: 'edit-text'
       },
     {
       cname: 'Divya Rathod',
       rno: '3367',
-      icon: <GrIcons.GrAddCircle />,
+      icon: <FiEdit2></FiEdit2>,
       cName: 'edit-text'
       },
     {
       cname: 'Neha Singh',
       rno: '3367',
-      icon: <GrIcons.GrAddCircle />,
+      icon: <FiEdit2></FiEdit2>,
       cName: 'edit-text'
       }
   ];
